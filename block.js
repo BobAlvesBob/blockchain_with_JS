faad7c13a5f22cef3b3276f1763e3299f11c4239
Bob <bobpitaia@gmail.com>
2024-12-08 17:15:24 +0000

    Creating the Block class in test

--- block.js
+++ block.js
@@ -1,19 +1,12 @@
 class Block {
-    constructor({timestamp, lastHash, hash, data}) {
-        this.timestamp = timestamp;
+     constructor({timestamp, lastHash, hash, data}) {
+       this.timestamp = timestamp;
         this.lastHash = lastHash;
-        this.hash = hash;
+       this.hash = hash;
         this.data = data;
     }
 
 }
 
-const block1 = new Block({
-    timestamp: "01/01/2022",
-    lastHash: "lastHash",
-    hash: "hash",
-    data: "data"
-});
+module.exports = Block;
 
-
-console.log('block1', block1);
